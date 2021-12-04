4c756b144e028b4d457693ef8ade9211996a3642
cugone <cugone@gmail.com>
2021-12-04 13:51:02 -0600

    4. Rays, a Simple Camera, and Background
    4.2 Sending Rays Into the Scene
    Listing 9: Rendering a blue-to-white gradient

--- RayTracingInOneWeekend/main.cpp
+++ RayTracingInOneWeekend/main.cpp
@@ -1,18 +1,41 @@
 #include "Color.hpp"
 #include "Vector3.hpp"
+#include "Ray3.hpp"
 
 #include <iostream>
 #include <string>
 
+Color ray_color(const Ray3& r);
+
 int main(int argc, char** argv) {
 
+    //Image
+    float aspect_ratio = 16.0f / 9.0f;
     const int image_width = [argc, argv]() -> const int {
-        return argc > 1 ? static_cast<int>(std::stoll(argv[1])) : 256u;
+        return argc > 1 ? static_cast<int>(std::stoll(argv[1])) : 400;
     }();
-    const int image_height = [argc, argv]() -> const int {
-        return argc > 2 ? static_cast<int>(std::stoll(argv[2])) : 256u;
+    const int image_height = [argc, argv, image_width, &aspect_ratio]() -> const int {
+        if(argc > 2) {
+            const int h = static_cast<int>(std::stoll(argv[2]));
+            aspect_ratio = image_width / static_cast<float>(h);
+            return h;
+        } else {
+            return static_cast<int>(image_width / aspect_ratio);
+        }
     }();
 
+    //Camera
+    const auto viewport_height = 2.0f;
+    const auto viewport_width = aspect_ratio * viewport_height;
+    const auto focal_length = 1.0f;
+
+    const auto origin = Point3{0.0f, 0.0f, 0.0f};
+    const auto horizontal = Vector3{viewport_width, 0, 0};
+    const auto vertical = Vector3{0, viewport_height, 0};
+    const auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vector3{0, 0, focal_length};
+
+
+    //Render
     const int max_pixel_value = 255;
 
     std::cout << "P3\n" << image_width << ' ' << image_height << '\n' << max_pixel_value << '\n';
@@ -20,10 +43,19 @@ int main(int argc, char** argv) {
     for(int y = image_height - 1; y >= 0; --y) {
         std::cerr << "\rScanlines remaining: " << y << ' ' << std::flush;
         for(int x = 0; x < image_width; ++x) {
-            Color pixel_color(static_cast<float>(x) / (image_width - 1), static_cast<float>(y) / (image_height - 1), 0.25f);
+            const auto u = static_cast<float>(x) / (image_width - 1);
+            const auto v = static_cast<float>(y) / (image_height - 1);
+            Ray3 r{origin, lower_left_corner + u * horizontal + v * vertical - origin};
+            Color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
     std::cerr << "\nDone.\n";
     return 0;
+}
+
+Color ray_color(const Ray3& r) {
+    Vector3 direction = unit_vector(r.direction());
+    auto t = 0.5f * (direction.y() + 1.0f);
+    return (1.0f - t) * Color(1.0f, 1.0f, 1.0f) + t * Color(0.5f, 0.7f, 1.0f);
 }
