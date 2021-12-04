2dbf4a872fdd786146389a50c0b19bcf5507dbbc
cugone <cugone@gmail.com>
2021-12-04 12:02:26 -0600

    3. The vec3 Class
    3.3 Color Utility Functions
    Listing 7: Final code for first PPM image

--- RayTracingInOneWeekend/main.cpp
+++ RayTracingInOneWeekend/main.cpp
@@ -1,3 +1,6 @@
+#include "Color.hpp"
+#include "Vector3.hpp"
+
 #include <iostream>
 #include <string>
 
@@ -17,15 +20,8 @@ int main(int argc, char** argv) {
     for(int y = image_height - 1; y >= 0; --y) {
         std::cerr << "\rScanlines remaining: " << y << ' ' << std::flush;
         for(int x = 0; x < image_width; ++x) {
-            const auto r = static_cast<double>(x) / (image_width - 1);
-            const auto g = static_cast<double>(y) / (image_height - 1);
-            const auto b = 0.25;
-
-            const auto ir = static_cast<int>((max_pixel_value + 1) * 0.999999 * r);
-            const auto ig = static_cast<int>((max_pixel_value + 1) * 0.999999 * g);
-            const auto ib = static_cast<int>((max_pixel_value + 1) * 0.999999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            Color pixel_color(static_cast<float>(x) / (image_width - 1), static_cast<float>(y) / (image_height - 1), 0.25f);
+            write_color(std::cout, pixel_color);
         }
     }
     std::cerr << "\nDone.\n";
