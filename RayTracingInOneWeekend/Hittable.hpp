08aec6a9c4a6518a155e133d3f20b2c9779a6d2d
cugone <cugone@gmail.com>
2021-12-04 14:36:39 -0600

    6. Surface Normals and Multiple Objects
    6.4 Front Faces Versus Back Faces
    Listing 18: Adding front-face tracking to hit_record

--- RayTracingInOneWeekend/Hittable.hpp
+++ RayTracingInOneWeekend/Hittable.hpp
@@ -4,10 +4,16 @@
 #include "Vector3.hpp"
 
 struct hit_record {
-    bool hit{false};
     Point3 p{};
     Vector3 normal{};
     float t;
+    bool hit{false};
+    bool front_face{ false };
+
+    inline void set_face_normal(const Ray3& r, const Vector3& outward_normal) {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 
 class Hittable {
