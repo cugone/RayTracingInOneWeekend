6d57e291251490fa5bdc99fbd1d4ee82c7ff0bd5
cugone <cugone@gmail.com>
2021-12-04 11:55:29 -0600

    3. The vec3 Class
    3.2 vec3 Utility Functions

--- RayTracingInOneWeekend/Vector3.hpp
+++ RayTracingInOneWeekend/Vector3.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cmath>
+#include <ostream>
+
 class Vector3 {
 public:
     Vector3() = default;
@@ -35,12 +38,12 @@ public:
         return *this *= 1.0f / t;
     }
 
-    float length() const {
-        return std::sqrt(length_squared());
-    }
-
     float length_squared() const {
         return m_e[0] * m_e[0] + m_e[1] * m_e[1] + m_e[2] * m_e[2];
+    }
+
+    float length() const {
+        return std::sqrt(length_squared());
     }
 
 protected:
@@ -50,3 +53,45 @@ private:
 
 using Point3 = Vector3;
 using Color = Vector3;
+
+inline std::ostream& operator<<(std::ostream& out, const Vector3& v) {
+    return out << v.x() << ' ' << v.y() << ' ' << v.z();
+}
+
+inline Vector3 operator+(const Vector3& u, const Vector3& v) {
+    return Vector3{ u.x() + v.x(), u.y() + v.y(), u.z() + v.z() };
+}
+
+inline Vector3 operator-(const Vector3& u, const Vector3& v) {
+    return Vector3{u.x() - v.x(), u.y() - v.y(), u.z() - v.z()};
+}
+
+inline Vector3 operator*(const Vector3& u, const Vector3& v) {
+    return Vector3{u.x() * v.x(), u.y() * v.y(), u.z() * v.z()};
+}
+
+inline Vector3 operator*(float t, const Vector3& v) {
+    return Vector3{t * v.x(), t * v.y(), t * v.z()};
+}
+
+inline Vector3 operator*(const Vector3& v, float t) {
+    return t * v;
+}
+
+inline Vector3 operator/(const Vector3& v, float t) {
+    return (1 / t) * v;
+}
+
+inline float dot(const Vector3& u, const Vector3& v) {
+    return u.x() * v.x() + u.y() * v.y() + u.z() * v.z();
+}
+
+inline Vector3 cross(const Vector3& u, const Vector3& v) {
+    return Vector3{u.y() * v.z() - u.z() * v.y(),
+                   u.z() * v.x() - u.x() * v.z(),
+                   u.x() * v.y() - u.y() * v.x()};
+}
+
+inline Vector3 unit_vector(Vector3 v) {
+    return v / v.length();
+}
