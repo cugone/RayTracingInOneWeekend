91330d21cdceb2d15c9cd03ad38f4026cd527448
cugone <cugone@gmail.com>
2021-12-05 10:27:03 -0600

    7. Antialiasing
    7.1 Some Random Number Utilities
    Listing 26 random_float(), alternate implementation

--- RayTracingInOneWeekend/MathUtils.hpp
+++ RayTracingInOneWeekend/MathUtils.hpp
@@ -1,15 +1,21 @@
 #pragma once
 
-#include <cmath>
 #include <climits>
+#include <cmath>
 #include <numbers>
-
+#include <random>
 
 constexpr auto infinity = std::numeric_limits<float>::infinity();
 constexpr auto pi = std::numbers::pi_v<float>;
 
 inline float degrees_to_radians(float degrees) {
     return degrees * pi / 180.0f;
+}
+
+inline float random_float() {
+    static std::uniform_real_distribution<float> d(0.0f, 1.0f);
+    static std::mt19937 g;
+    return d(g);
 }
 
 #include "Ray3.hpp"
