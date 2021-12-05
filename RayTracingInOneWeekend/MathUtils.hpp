5f2b9cf371447b6628cd9a16e97256cd3768dd60
cugone <cugone@gmail.com>
2021-12-05 11:18:45 -0600

    Add missing random_float with arguments

--- RayTracingInOneWeekend/MathUtils.hpp
+++ RayTracingInOneWeekend/MathUtils.hpp
@@ -13,7 +13,13 @@ inline float degrees_to_radians(float degrees) {
 }
 
 inline float random_float() {
-    static std::uniform_real_distribution<float> d(0.0f, 1.0f);
+    std::uniform_real_distribution<float> d(0.0f, 1.0f);
+    static std::mt19937 g;
+    return d(g);
+}
+
+inline float random_float(float min, float exclusive_max) {
+    std::uniform_real_distribution<float> d(min, exclusive_max);
     static std::mt19937 g;
     return d(g);
 }
