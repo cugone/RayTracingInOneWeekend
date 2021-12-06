812cb14397de0d37fcb304e846b7be7ca040ccf6
cugone <cugone@gmail.com>
2021-12-06 13:22:52 -0600

    13. Where Next?
    13.1 A Final Render

--- RayTracingInOneWeekend/main.cpp
+++ RayTracingInOneWeekend/main.cpp
@@ -13,10 +13,12 @@
 Color ray_color(const Ray3& r, const Hittable& world, int depth);
 float hit_sphere(const Point3& center, float radius, const Ray3& r);
 
+HittableList random_scene();
+
 int main(int argc, char** argv) {
 
     //Image
-    float aspect_ratio = 16.0f / 9.0f;
+    float aspect_ratio = 3.0f / 2.0f;
     const int image_width = [argc, argv]() -> const int {
         return argc > 1 ? static_cast<int>(std::stoll(argv[1])) : 400;
     }();
@@ -38,25 +40,14 @@ int main(int argc, char** argv) {
 
 
     //World
-    HittableList world{};
-
-    auto material_ground = make_lambertian(MaterialDesc{ Color{0.8f, 0.8f, 0.0f} });
-    auto material_center = make_lambertian(MaterialDesc{Color{0.1f, 0.2f, 0.5f}});
-    auto material_left = make_dielectric(MaterialDesc{ Color{1.0f, 1.0f, 1.0f}, 0.0f, 0.0f, 1.5f });
-    auto material_right = make_metal(MaterialDesc{ Color{0.8f, 0.6f, 0.2f}, 0.0f, 1.0f });
-
-    world.add(std::make_shared<Sphere3>(Point3{0.0f, -100.5f, -1.0f}, 100.0f, material_ground));
-    world.add(std::make_shared<Sphere3>(Point3{0.0f, 0.0f, -1.0f}, 0.5f, material_center));
-    world.add(std::make_shared<Sphere3>(Point3{-1.0f, 0.0f, -1.0f}, 0.5f, material_left));
-    world.add(std::make_shared<Sphere3>(Point3{-1.0f, 0.0f, -1.0f}, -0.45f, material_left));
-    world.add(std::make_shared<Sphere3>(Point3{1.0f, 0.0f, -1.0f}, 0.5f, material_right));
+    HittableList world = random_scene();
 
     //Camera
-    const auto lookFrom = Point3{3.0f, 3.0f, 2.0f};
-    const auto lookAt = Point3{0.0f, 0.0f, -1.0f};
+    const auto lookFrom = Point3{13.0f, 2.0f, 3.0f};
+    const auto lookAt = Point3{0.0f, 0.0f, 0.0f};
     const auto vUp = Vector3{0.0f, 1.0f, 0.0f};
-    const auto distance_to_focus = (lookFrom - lookAt).length();
-    const auto aperture = 2.0f;
+    const auto distance_to_focus = 10.0f;
+    const auto aperture = 0.1f;
 
     Camera camera{lookFrom, lookAt, vUp, 20, aspect_ratio, aperture, distance_to_focus};
 
@@ -114,4 +105,48 @@ float hit_sphere(const Point3& center, float radius, const Ray3& r) {
     } else {
         return (-half_b - std::sqrt(discriminant)) / a;
     }
+}
+
+HittableList random_scene() {
+    HittableList world{};
+
+    const auto ground_material = make_lambertian(MaterialDesc{ Color{0.5f, 0.5f, 0.5f} });
+    world.add(std::make_shared<Sphere3>(Point3{0.0f, -1000.0f, 0.0f}, 1000.0f, ground_material));
+
+    for(int a = -11; a < 11; ++a) {
+        for(int b = -11; b < 11; ++b) {
+            const auto choose_mat = random_float();
+            const auto center = Point3{a + 0.9f * random_float(), 0.2f, b + 0.9f * random_float()};
+
+            if((center - Point3{4.0f, 0.2f, 0.0f}).length() > 0.9f) {
+                Material material{};
+                MaterialDesc desc{};
+                if(choose_mat < 0.8f) {
+                    desc.color = Color::random() * Color::random();
+                    material = make_lambertian(desc);
+                } else if(choose_mat < 0.95f) {
+                    desc.color = Color::random(0.5f, 1.0f);
+                    desc.roughness = random_float(0.0f, 0.5f);
+                    desc.metallic = 1.0f;
+                    material = make_metal(desc);
+                } else {
+                    desc.refractionIndex = 1.5f;
+                    desc.color = Color{1.0f, 1.0f, 1.0f};
+                    desc.roughness = 0.0f;
+                    material = make_dielectric(desc);
+                }
+                world.add(std::make_shared<Sphere3>(center, 0.2f, material));
+            }
+        }
+    }
+
+    const auto glass = make_dielectric(MaterialDesc{ Color{1.0f, 1.0f, 1.0f}, 0.0f, 0.0f, 1.5f });
+    const auto lambertian = make_lambertian(MaterialDesc{ Color{0.4f, 0.2f, 0.1f}});
+    const auto metal = make_metal(MaterialDesc{ Color{0.7f, 0.6f, 0.5f}, 0.0f, 1.0f});
+
+    world.add(std::make_shared<Sphere3>(Point3{0.0f, 1.0f, 0.0f}, 1.0f, glass));
+    world.add(std::make_shared<Sphere3>(Point3{-4.0f, 1.0f, 0.0f}, 1.0f, lambertian));
+    world.add(std::make_shared<Sphere3>(Point3{4.0f, 1.0f, 0.0f}, 1.0f, metal));
+
+    return world;
 }
