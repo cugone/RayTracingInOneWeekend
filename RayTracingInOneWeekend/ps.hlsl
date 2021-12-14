674c32a6811217ec407510a11ce1f7f1dfe1b3a7
cugone <cugone@gmail.com>
2021-12-14 10:59:19 -0600

    Added constant buffers to hold world, frame, and object data

--- RayTracingInOneWeekend/ps.hlsl
+++ RayTracingInOneWeekend/ps.hlsl
@@ -4,6 +4,25 @@ struct ps_input {
 	float2 uv : TEXCOORD;
 };
 
+cbuffer world_constants : register(b0) {
+	float gScreenWidth;
+	float gScreenHeight;
+};
+
+cbuffer frame_constants : register(b1) {
+	float4x4 gMVPMatrix;
+	float4x4 gViewMatrix;
+	float4x4 gProjectionMatrix;
+	float gGameTime;
+	float gGameFrameTime;
+	float gSamplesPerPixel;
+	float gMaxDepth;
+};
+
+cbuffer object_constants : register(b2) {
+	float4x4 gModelMatrix;
+};
+
 float4 main(ps_input input) : SV_TARGET {
 	return input.color;
 }
