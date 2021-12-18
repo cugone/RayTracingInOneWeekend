b05bf951c32cb3fdd1e95a99ffc2a144b69ef76b
cugone <cugone@gmail.com>
2021-12-18 15:31:17 -0600

    Add constant buffers for world, frame, and scene

--- RayTracingInOneWeekend/vs.hlsl
+++ RayTracingInOneWeekend/vs.hlsl
@@ -10,13 +10,33 @@ struct vs_input {
     float2 uv : UV;
 };
 
+cbuffer world_constants : register(b0) {
+    float gScreenWidth;
+    float gScreenHeight;
+};
+
+cbuffer frame_constants : register(b1) {
+    float4x4 gMVPMatrix;
+    float4x4 gViewMatrix;
+    float4x4 gProjectionMatrix;
+    float gGameTime;
+    float gGameFrameTime;
+    float gSamplesPerPixel;
+    float gMaxDepth;
+};
+
+cbuffer object_constants : register(b2) {
+    float4x4 gModelMatrix;
+};
+
 ps_input main(vs_input input) {
     ps_input output;
 
     float4 pos = float4(input.pos, 1.0f);
     float4 color = float4(input.color, 1.0f);
 
-    output.position = pos;
+    output.position = mul(pos, gMVPMatrix);
+
     output.color = color;
     output.uv = input.uv;
 
